cec4d9ddb31021824dfee8f61bccf602d505f9d0
Mino <126659815+minoxiii@users.noreply.github.com>
2026-04-05 16:17:25 +0545

    Playermovement
    Playermovement script before finite state machine

--- Player.cs
+++ Player.cs
@@ -1,35 +1,150 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    [Header("Components")]
     public Rigidbody2D rb;
-    public float speed;
     public PlayerInput playerInput;
-    public Vector2 moveInput;
-    public int facingDirection=1;
+    public Animator anim;
 
-    void Update(){
-        Flip();
+    [Header("Movement Variables")]
+    public float walkSpeed;
+    public float runSpeed;
+    public float jumpForce;
+    public float jumpCutMultiplier = .5f;
+    public float normalGravity;
+    public float fallGravity;
+    public float jumpGravity;
+
+    public int facingDirection = 1;
+    //Inputs
+    private Vector2 moveInput;
+    private bool runPressed;
+    private bool jumpPressed;
+    private bool jumpReleased;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rb.gravityScale = normalGravity;
     }
 
-    void FixedUpdate(){
-        float targetSpeed = moveInput.x * speed;
-        rb.linearVelocity = new Vector2(targetSpeed,rb.linearVelocity.y);
+    void Update()
+    {
+        Flip();
+        HandleAnimations();
+    }
+
+    void FixedUpdate()
+    {
+        ApplyVariableGravity();
+        CheckGrounded();
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement()
+    {
+        float currentSpeed = runPressed ? runSpeed : walkSpeed;
+        float targetSpeed = moveInput.x * currentSpeed;
+        rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);
+    }
+
+    private void HandleJump()
+    {
+        if (jumpPressed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpPressed = false;
+            jumpReleased = false; //reset jump input
+        }
+        else if (jumpReleased && rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+            jumpReleased = false; //reset jump release input
+        }
+    }
+
+    void ApplyVariableGravity()
+    {
+        if (rb.linearVelocity.y > 0.1f) //rising
+        {
+            rb.gravityScale = jumpGravity;
+        }
+        else if (rb.linearVelocity.y < -0.1f) //falling
+        {
+            rb.gravityScale = fallGravity;
+        }
+        else //not moving vertically
+        {
+            rb.gravityScale = normalGravity;
+        }
+    }
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void HandleAnimations()
+    {
+        anim.SetBool("isJumping", rb.linearVelocity.y > .1f);
+        anim.SetBool("isGrounded", isGrounded);
+
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+
+        bool isMoving = Mathf.Abs(moveInput.x) > .1f &&isGrounded;
+
+        anim.SetBool("isWalking", isMoving && !runPressed);
+        anim.SetBool("isRunning", isMoving && runPressed);
     }
 
     void Flip()
     {
-        if(moveInput.x>0.1f){
-    facingDirection=1;
-    }
-    else if(moveInput.x<-0.1f){
-        facingDirection=-1;
-    }
-    transform.localScale=new Vector3(facingDirection,1,1);
+        if (moveInput.x > 0.1f)
+        {
+            facingDirection = 1;
+        }
+        else if (moveInput.x < -0.1f)
+        {
+            facingDirection = -1;
+        }
+        transform.localScale = new Vector3(facingDirection, 1, 1);
     }
 
-    public void OnMove(InputValue value){
+    public void OnMove(InputValue value)
+    {
+        Debug.Log("MOVE CALLED");  
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnRun(InputValue value)
+    {
+        runPressed = value.isPressed;
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed && isGrounded)
+        {
+            jumpPressed = true;
+            jumpReleased = false;
+        }
+        else //button released
+        {
+            jumpReleased = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
